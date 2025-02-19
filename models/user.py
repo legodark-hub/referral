798d1419f1a5ed7337e4b0b569a72198692b7ad5
aidarminyazev <legodark@mail.ru>
2025-02-20 00:24:49 +0500

    add user model and schemas with SQLAlchemy and Pydantic

---
+++ models/user.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from models.base import Base
+from utils.custom_types import created_at, updated_at
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    referral_code: Mapped[str] = mapped_column(String(6), unique=True, nullable=True)
+    referrer_id: Mapped[int | None] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=True
+    )
+    referrer = relationship(
+        "User", remote_side=[id], backref="referrals", uselist=False
+    )
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
+    def __repr__(self) -> str:
+        return f"<User {self.username}>"
