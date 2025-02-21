5241061831e683c048116718f0d5ce654304412a
aidarminyazev <legodark@mail.ru>
2025-02-22 01:02:50 +0500

    enhance user creation process with password hashing and referral code validation

--- api/services/user.py
+++ api/services/user.py
@@ -1,4 +1,8 @@
 from typing import Any
+
+from fastapi import HTTPException, status
+from schemas.user import UserCreate
+from utils.security import get_password_hash
 from utils.service import BaseService
 from utils.uow import transaction_mode
 
@@ -7,8 +11,20 @@ class UserService(BaseService):
     base_repository = 'user'
     
     @transaction_mode
-    async def create_user(self, user_data: dict) -> Any:
-        return await self.uow.user.create_user(user_data)
+    async def create_user(self, user_data: UserCreate) -> Any:
+        user_dict = user_data.model_dump()
+        user_dict["password_hash"] = get_password_hash(user_dict.pop("password"))
+        
+        referral_code = user_dict.pop("referral_code", None)
+        if referral_code:
+            referrer = await self.uow.referral_code.get_by_code(referral_code)
+            if not referrer:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid referral code"
+                )
+            
+        
+        return await self.uow.user.create_user(user_dict)
 
     @transaction_mode
     async def get_user_by_id(self, user_id: int) -> Any:
