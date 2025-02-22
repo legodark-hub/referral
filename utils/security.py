56e804a8b9047184709ad95597a7ee188c87fd9e
aidarminyazev <legodark@mail.ru>
2025-02-22 19:06:28 +0500

    move AuthX configuration to security module and clean up main application entry point

--- utils/security.py
+++ utils/security.py
@@ -1,6 +1,15 @@
+from authx import AuthX, AuthXConfig
 from passlib.context import CryptContext
 
+from config import JWT_SECRET_KEY
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+config = AuthXConfig()
+config.JWT_ALGORITHM = "HS256"
+config.JWT_SECRET_KEY = JWT_SECRET_KEY
+
+security = AuthX(config=config)
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
