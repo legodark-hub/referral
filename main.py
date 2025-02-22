c411e1c41e04c9678bf242e024ff25e98cf6ceab
aidarminyazev <legodark@mail.ru>
2025-02-23 01:02:40 +0500

    refactor: rename user router import and add referral code router

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from api.routers.user import router
+from api.routers.user import router as user_router
+from api.routers.referral_code import router as referral_code_router
 
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(user_router)
+app.include_router(referral_code_router)
 
