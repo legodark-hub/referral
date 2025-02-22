58938178605f2ec31e8884053ac8d934a29949cf
aidarminyazev <legodark@mail.ru>
2025-02-23 01:02:15 +0500

    add user profile retrieval and protected route endpoints

--- api/routers/user.py
+++ api/routers/user.py
@@ -1,6 +1,8 @@
+from authx import TokenPayload
 from fastapi import APIRouter, Depends, HTTPException, status
 from schemas.user import Token, UserCreate, UserLogin, UserResponse
 from api.services.user import UserService
+from utils.security import security
 
 router = APIRouter(prefix="/user")
 
@@ -23,7 +25,21 @@ async def register_user(
     user = await service.create_user(user_data)
     return UserResponse(payload=user)
 
+
 @router.post("/login")
 async def login(user_data: UserLogin, service: UserService = Depends(UserService)):
     token = await service.authenticate_user(user_data)
     return Token(access_token=token, token_type="bearer")
+
+
+@router.get("/me", response_model=UserResponse)
+async def get_me(
+    service: UserService = Depends(UserService),
+    payload: TokenPayload = Depends(security.access_token_required),
+):
+    return UserResponse(payload=await service.get_user_by_id(payload.sub))
+
+
+@router.get("/protected", dependencies=[Depends(security.access_token_required)])
+def get_protected():
+    return {"message": "Hello World"}
